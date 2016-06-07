debd3239479e47b2ec560cb1d4bc560108f33e57
Cameron Dutro <camertron@gmail.com>
2016-06-07 16:52:05 -0700

    Add tests

--- lib/txgh/config/tx_manager.rb
+++ lib/txgh/config/tx_manager.rb
@@ -7,14 +7,7 @@ module Txgh
         def tx_config(transifex_project, github_repo, ref = nil)
           options = { github_repo: github_repo, ref: ref }
           scheme, payload = split_uri(transifex_project.tx_config_uri)
-
-          if provider = provider_for(scheme)
-            provider.load(payload, options)
-          else
-            raise Txgh::InvalidProviderError,
-              "Couldn't find a provider for the '#{scheme}' scheme. Please "\
-              "make sure txgh is configured properly."
-          end
+          provider_for(scheme).load(payload, options)
         end
       end
     end
