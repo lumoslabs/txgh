199d5e4e5f2b05f5899a959301ddbf69065c721a
Cameron Dutro <camertron@gmail.com>
2016-08-17 15:26:24 -0700

    Download from github using the contents API

--- txgh/lib/txgh/github_api.rb
+++ txgh/lib/txgh/github_api.rb
@@ -90,13 +90,12 @@ module Txgh
     end
 
     def download(repo, path, branch)
-      master = client.ref(repo, branch)
-      commit = client.commit(repo, master[:object][:sha])
-      tree = client.tree(repo, commit[:commit][:tree][:sha], recursive: 1)
+      contents = client.contents(repo, { path: path, ref: branch })
 
-      if found = tree[:tree].find { |t| t[:path] == path }
-        b = blob(repo, found[:sha])
-        b['encoding'] == 'utf-8' ? b['content'] : Base64.decode64(b['content'])
+      if contents[:encoding] == 'utf-8'
+        contents[:content]
+      else
+        Base64.decode64(contents[:content])
       end
     end
 
