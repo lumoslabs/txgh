8a089cca97df86ad9044fea758cd95de08c0a67c
Cameron Dutro <camertron@gmail.com>
2016-10-06 16:51:27 -0700

    Moar specs

--- server/spec/webhooks/github/request_handler_spec.rb
+++ server/spec/webhooks/github/request_handler_spec.rb
@@ -75,48 +75,107 @@ describe RequestHandler do
   end
 
   describe '#enqueue' do
-    let(:queue_config) do
-      {
-        backend: 'test',
-        options: {
-          queues: %w(test-queue)
+    let(:payload) { { repository: { full_name: repo_name } } }
+    let(:event) { 'push' }
+
+    it "responds with an error when a queue backend isn't configured" do
+      allow(handler).to receive(:authentic_request?).and_return(true)
+      response = handler.enqueue
+      expect(response.body.first[:error]).to eq('Queue not configured')
+      expect(response.status).to eq(500)
+    end
+
+    context 'with a queue configured' do
+      let(:queue_config) do
+        {
+          backend: 'test',
+          options: {
+            queues: %w(test-queue)
+          }
         }
-      }
-    end
-
-    let(:backend) { TxghQueue::Config.backend }
-    let(:producer) { backend.producer_for(event) }
-
-    before(:each) do
-      allow(TxghQueue::Config).to(
-        receive(:raw_config).and_return(queue_config)
-      )
-    end
-
-    after(:each) { backend.reset! }
-
-    context 'delete event' do
-      let(:event) { 'delete' }
-      let(:payload) { GithubPayloadBuilder.delete_payload(repo_name, ref) }
-
-      it 'does not enqueue if unauthorized' do
-        expect { handler.enqueue }.to_not change { producer.enqueued_jobs.size }
       end
 
-      it 'responds with an unauthorized status' do
-        response = handler.enqueue
-        expect(response.status).to eq(401)
+      let(:backend) { TxghQueue::Config.backend }
+      let(:producer) { backend.producer_for("github.#{event}") }
+
+      before(:each) do
+        allow(TxghQueue::Config).to(
+          receive(:raw_config).and_return(queue_config)
+        )
       end
-    end
 
-    context 'unrecognized event' do
-      let(:event) { 'foo' }
-      let(:payload) { { repository: { full_name: repo_name } } }
+      after(:each) { backend.reset! }
 
-      it 'responds with a 400' do
-        allow(handler).to receive(:authentic_request?).and_return(true)
-        response = handler.enqueue
-        expect(response.status).to eq(400)
+      context 'push event' do
+        let(:event) { 'push' }
+        let(:payload) { GithubPayloadBuilder.push_payload(repo_name, ref).tap { |p| p.add_commit } }
+
+        it 'does not enqueue if unauthorized' do
+          expect { handler.enqueue }.to_not change { producer.enqueued_jobs.size }
+        end
+
+        it 'responds with an unauthorized status' do
+          response = handler.enqueue
+          expect(response.status).to eq(401)
+        end
+
+        context 'with an authentic request' do
+          before(:each) do
+            allow(handler).to receive(:authentic_request?).and_return(true)
+          end
+
+          it 'enqueues the job' do
+            expect { handler.enqueue }.to(
+              change { producer.enqueued_jobs.size }.from(0).to(1)
+            )
+          end
+
+          it 'responds with an ok status code' do
+            response = handler.enqueue
+            expect(response.status).to eq(200)
+          end
+        end
+      end
+
+      context 'delete event' do
+        let(:event) { 'delete' }
+        let(:payload) { GithubPayloadBuilder.delete_payload(repo_name, ref) }
+
+        it 'does not enqueue if unauthorized' do
+          expect { handler.enqueue }.to_not change { producer.enqueued_jobs.size }
+        end
+
+        it 'responds with an unauthorized status' do
+          response = handler.enqueue
+          expect(response.status).to eq(401)
+        end
+
+        context 'with an authentic request' do
+          before(:each) do
+            allow(handler).to receive(:authentic_request?).and_return(true)
+          end
+
+          it 'enqueues the job' do
+            expect { handler.enqueue }.to(
+              change { producer.enqueued_jobs.size }.from(0).to(1)
+            )
+          end
+
+          it 'responds with an ok status code' do
+            response = handler.enqueue
+            expect(response.status).to eq(200)
+          end
+        end
+      end
+
+      context 'unrecognized event' do
+        let(:event) { 'foo' }
+
+        it 'responds with a 400' do
+          allow(handler).to receive(:authentic_request?).and_return(true)
+          response = handler.enqueue
+          expect(response.status).to eq(400)
+        end
       end
     end
   end
